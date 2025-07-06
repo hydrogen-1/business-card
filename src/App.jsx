26ce86dc71dd4069469d4fc0ffa6f66007f63287
Johannes Becker <johannes@becker.computer>
2025-07-06 20:52:54 +0200

    add interests section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Info from "./components/Info";
 import About from "./components/About";
+import Interests from "./components/Interests";
 
 function App() {
   return (
     <div className="content">
       <Info />
       <About />
+      <Interests />
     </div>
   );
 }
