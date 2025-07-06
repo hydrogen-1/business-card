39eae8ceee397be78f98d620ccd198733192be9d
Johannes Becker <johannes@becker.computer>
2025-07-06 14:37:48 +0200

    add info section

---
+++ src/components/Info.jsx
@@ -0,0 +1,15 @@
+function Info() {
+  return (
+    <header>
+      <img src="/img/anzug.jpeg" alt="Foto von Johannes Becker" />
+      <h1>Johannes Becker</h1>
+      <h2>Fullstack Developer</h2>
+      <button>
+        <img src="/logo/email.svg" alt="email logo" />
+        E-Mail
+      </button>
+    </header>
+  );
+}
+
+export default Info;
