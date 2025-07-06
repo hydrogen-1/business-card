43ed82630ce93a5c6250064938d02496db5f1dea
Johannes Becker <johannes@becker.computer>
2025-07-06 19:29:45 +0200

    add about section

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>
+        I am a fullstack developer with a particular interest in being green and
+        stopping waste. I always try to learn and apply best practices and don't
+        shy away from learning new practices
+      </p>
+    </div>
+  );
+}
+
+export default About;
